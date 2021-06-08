22c88d1a46cef716e200bc7e8e38f8f802f0fd41
paritadey <paritasampa95@gmail.com>
2021-06-08 12:59:05 +0530

    Test parts added--parita

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -5,15 +5,22 @@ import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
@@ -28,4 +35,26 @@ class ReminderListFragmentTest {
 //    TODO: test the navigation of the fragments.
 //    TODO: test the displayed data on the UI.
 //    TODO: add testing for the error messages.
+
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(RemindersActivity::class.java)
+            .onActivity { it.supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.nav_host_fragment,ReminderListFragment())}
+    }
+    @Test
+    fun errorMessageTest(){
+
+        onView(withId(R.id.saveReminder)).perform(click())
+        onView(withId(R.id.reminderTitle)).check(ViewAssertions.matches(ViewMatchers.hasErrorText("Field cannot be left empty.")))
+        onView(withId(R.id.reminderDescription)).check(
+            ViewAssertions.matches(
+                ViewMatchers.hasErrorText(
+                    "Field cannot be left empty."
+                )
+            )
+        )
+    }
+
 }
--- starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,9 +1,31 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.app.Application
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.TestCoroutineRule
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
+import io.mockk.MockKAnnotations
+import junit.framework.Assert.assertNotNull
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
 import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
@@ -11,6 +33,41 @@ class SaveReminderViewModelTest {
 
 
     //TODO: provide testing to the SaveReminderView and its live data objects
+    @Rule
+    @JvmField
+    val rule: TestRule = InstantTaskExecutorRule()
 
+    @Rule
+    val testCoroutineRule = TestCoroutineRule()
+
+    @Mock
+    private lateinit var dataSource: ReminderDataSource
+
+    @Mock
+    private lateinit var showLoading: Observer<Boolean>
+    @Mock
+    private lateinit var tasksViewModel:SaveReminderViewModel
+
+    @Before
+    fun setupViewModel() {
+         tasksViewModel = SaveReminderViewModel(ApplicationProvider.getApplicationContext(), dataSource)
+    }
+
+    @Test
+    fun saveReminder_shouldReturnSuceess() {
+        val data = ReminderDataItem(
+            "title",
+            "description",
+            "location",
+            22.8869,
+            88.657,
+            "id"
+        )
+        testCoroutineRule.runBlockingTest {
+            tasksViewModel.validateAndSaveReminder(data)
+            tasksViewModel.showLoading.observeForever(showLoading)
+        }
+
+    }
 
 }
