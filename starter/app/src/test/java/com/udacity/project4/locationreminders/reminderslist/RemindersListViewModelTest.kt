d55339bbed37b3ff410f5aa8d1052f290832e3ba
souvik <souvikdatta900@gmail.com>
2021-06-12 20:57:36 +0530

    Changes

--- starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,8 +1,17 @@
 package com.udacity.project4.locationreminders.reminderslist
 
 import android.app.Application
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.*
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.MyApp
+import com.udacity.project4.locationreminders.data.FakeDataSource
 import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.local.RemindersDao
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.*
@@ -18,18 +27,22 @@ import org.mockito.MockitoAnnotations
 class RemindersListViewModelTest {
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
-/*
-    @Rule
-    var instantExecutorRule: InstantTaskExecutorRule = InstantTaskExecutorRule()
-*/
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private var viewModel: RemindersListViewModel? = null
+    var fakeDataSource: FakeDataSource? = null
+    lateinit var data: ReminderDTO
+    lateinit var reminderListViewModel: RemindersListViewModel
+    lateinit var reminderRepository: RemindersLocalRepository
+
 
     @Mock
     var dataSource: ReminderDataSource? = null
 
-//    @Mock
-//    var observer: Observer<List<ReminderDataItem>?>? = null
+    @Mock
+    var remindersDao: RemindersDao? = null
+
 
     @Mock
     lateinit var lifecycleOwner: LifecycleOwner
@@ -38,22 +51,48 @@ class RemindersListViewModelTest {
     @Before
     @Throws(Exception::class)
     fun setUp() {
-//        MockitoAnnotations.initMocks(this)
-      //  lifecycle = LifecycleRegistry(lifecycleOwner)
+
         MockitoAnnotations.initMocks(this@RemindersListViewModelTest)
+        data = ReminderDTO(
+            id = "abcs",
+            title = "hello",
+            description = "world",
+            latitude = 22.8758,
+            longitude = 88.975,
+            location = "location"
+        )
+
         lifecycle = LifecycleRegistry(lifecycleOwner)
-        viewModel = dataSource?.let { RemindersListViewModel(Application(), it) }
-//        viewModel?.remindersList?.observeForever(observer)
+        viewModel = RemindersListViewModel(MyApp(), FakeDataSource())
+        reminderRepository = remindersDao?.let {
+            RemindersLocalRepository(
+                it,
+                Dispatchers.Unconfined
+            )
+        }!!
+        insertNote()
+    }
+
+    private fun insertNote() {
+        fakeDataSource = FakeDataSource(MutableList<ReminderDTO>(5) {
+            data
+        })
+        reminderListViewModel = RemindersListViewModel(
+            ApplicationProvider.getApplicationContext(),
+            fakeDataSource!!
+        )
+        runBlockingTest {
+            reminderRepository.saveReminder(data)
+        }
     }
 
     @Test
     fun testNull() {
         runBlockingTest {
             Mockito.`when`(
-                dataSource?.getReminders()).thenReturn(null)
+                dataSource?.getReminders()
+            ).thenReturn(null)
             Assert.assertNotNull(viewModel?.remindersList)
-           //Assert.assertTrue(viewModel?.remindersList?.hasObservers()==true)
-           //viewModel?.remindersList?.let { Assert.assertTrue(it.hasObservers()) }
 
         }
     }
@@ -62,7 +101,8 @@ class RemindersListViewModelTest {
     fun testFetchDataSuccess() {
         runBlockingTest {
             Mockito.`when`(
-                viewModel?.loadReminders()).thenReturn(null)
+                viewModel?.loadReminders()
+            ).thenReturn(null)
         }
     }
 
